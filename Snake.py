b09333e492702763b2682126d56423e91dbada62
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-10-15 10:31:16 -0700

    Made variables

---
+++ Snake.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+pygame.init()
+pygame.display.set_caption("Welcome to the game with a little twist!")
+
+
+screen-width = 650
+Screen-height = 650
+
+Character-width = 35
+Character-height = 35
