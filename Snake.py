33843b74211707206ac90d15d1fb0680cb382426
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-10-15 10:39:36 -0700

    added more variables and constants

--- Snake.py
+++ Snake.py
@@ -5,8 +5,17 @@ pygame.init()
 pygame.display.set_caption("Welcome to the game with a little twist!")
 
 
-screen-width = 650
+Screen-width = 650
 Screen-height = 650
 
 Character-width = 35
 Character-height = 35
+
+LimeGreen = (50,205,50)
+Black = (0,0,0)
+ShamrockGreen = (0,158,96)
+Red = (255,0,0)
+
+player_x = 65
+player_y = 65
+
