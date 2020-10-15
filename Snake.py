8d9483de39ebc13052cd1ab9c0accc3eb6c70b81
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-10-15 11:22:12 -0700

    made a function for design and a while loop to keep the game running

--- Snake.py
+++ Snake.py
@@ -16,6 +16,7 @@ Black = (0,0,0)
 ShamrockGreen = (0,158,96)
 Red = (255,0,0)
 
+
 player_x = 65
 player_y = 65
 
@@ -27,6 +28,31 @@ enemy_y = 10
 
 speed = 6
 points = 0
+timer = 0
 
 screen = pygame.display.set_mode([ScreenWidth, ScreenHeight])
+
+def is_touched(x1, y1, x2, y2, width, height):
+    if(x1 >= x2 + width) or (x2 >= x1 + width):
+         return False
+
+    if (y1 >= y2 + height) or (y2 >= y1 + height):
+         return False
+    return True
+
+def design(text, color, font_size, x, y):
+    font = pygame.font.SysFont(None, font_size)
+    img = font.render(text, True, color)
+    screen.blit(img, (x,y))
+
+running = True
+while running:
+
+    pygame.time.delay(20)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
 
