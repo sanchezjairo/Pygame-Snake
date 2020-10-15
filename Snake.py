9eb117bf8bb4c40c8ac4cdfc4b18ae2706955cc9
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-10-15 10:53:33 -0700

    Made an helpfer function when character touches other blocks

--- Snake.py
+++ Snake.py
@@ -5,11 +5,11 @@ pygame.init()
 pygame.display.set_caption("Welcome to the game with a little twist!")
 
 
-Screen-width = 650
-Screen-height = 650
+ScreenWidth = 650
+ScreenHeight = 650
 
-Character-width = 35
-Character-height = 35
+CharacterWidth = 35
+CharacterHeight = 35
 
 LimeGreen = (50,205,50)
 Black = (0,0,0)
@@ -18,4 +18,15 @@ Red = (255,0,0)
 
 player_x = 65
 player_y = 65
+
+food_x = 300
+food_y = 30
+
+enemy_x = 300
+enemy_y = 10
+
+speed = 6
+points = 0
+
+screen = pygame.display.set_mode([ScreenWidth, ScreenHeight])
 
